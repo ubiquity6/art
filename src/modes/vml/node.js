9e0f8cf868e82631a9c6406986a0cea9ce35c70b
Sebastian Markbage <sebastian@calyptus.eu>
2013-05-07 11:29:31 -0700

    Remove logs

--- src/modes/vml/node.js
+++ src/modes/vml/node.js
@@ -4,7 +4,7 @@ var Element = require('../../dom/shadow');
 var DOM = require('./dom');
 
 module.exports = Class(Element, Transform, {
-	
+
 	initialize: function(tag){
 		//this.uid = uniqueID();
 		var element = this.element = DOM.createElement(tag);
@@ -13,28 +13,24 @@ module.exports = Class(Element, Transform, {
 
 	_place: function(){
 		if (this.parentNode){
-			console.log('transforming');
 			this._transform();
-			var t = this._activeTransform;
-			if (t)
-			console.log([t.xx, t.xy, t.x].join(', '));
 		}
 	},
-	
+
 	// visibility
-	
+
 	hide: function(){
 		this.element.style.display = 'none';
 		return this;
 	},
-	
+
 	show: function(){
 		this.element.style.display = '';
 		return this;
 	},
-	
+
 	// interaction
-	
+
 	indicate: function(cursor, tooltip){
 		if (cursor) this.element.style.cursor = cursor;
 		if (tooltip) this.element.title = tooltip;
